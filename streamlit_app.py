d5ae08fd72b5a47edb1f404c801fb71d12bdef10
Elizabeth Rosso <data.application.builder@gmail.com>
2022-11-23 21:44:56 -0500

    Update streamlit_app.py
    Added a header and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('Data Application Using Snowflake and Streamlit')
+
+streamlit.header('Data Applications are Easy to Create Using Snowflake and Streamlit')
+streamlit.text('Data')
+streamlit.text('Application')
+streamlit.text('Snowflake')
+streamlit.text('Streamlit')
