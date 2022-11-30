59ee10b17a308e4dd6aca1952bf5a8dd56b1e5cb
Elizabeth Rosso <data.application.builder@gmail.com>
2022-11-30 13:30:13 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,6 @@ streamlit.dataframe(fruits_to_show)
 # New section to display FruityVice API response
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
 
 # Normalize the data from the json variable
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
